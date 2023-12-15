9011c300c19c4a22551796b419b53e243aa033dd
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-15 13:15:43 +0000

    change netlify deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h2>CI CD Tuturials </h2>
-      <p>Deploying a React app to Netlify</p>
+      <p>Deploying a React app to Netlify again</p>
     </>
   );
 }
